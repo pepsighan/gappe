33fc7d373891c474c67c0e195dadf9261357a57b
Sharad Chand <pepsighan@sharadchand.com>
2022-03-03 09:39:53 +0530

    Update the program id

--- programs/gappe/src/lib.rs
+++ programs/gappe/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7spG7C84YJyTaxVvHLLtNbWF18SgACLyz5AdkBrwU1zY");
 
 #[program]
 pub mod gappe {
