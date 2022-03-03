08dbd96e8a4111a256e8fd28b14ba4716d7acc27
Sharad Chand <pepsighan@sharadchand.com>
2022-03-03 09:09:59 +0530

    Add func to setup profile

--- programs/gappe/src/lib.rs
+++ programs/gappe/src/lib.rs
@@ -5,10 +5,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod gappe {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    /// Setup profile for an account.
+    pub fn setup_profile(ctx: Context<SetupProfile>, username: String) -> ProgramResult {
+        ctx.accounts.profile.username = username;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetupProfile<'info> {
+    /// `mut` is needed to save changes made to the profile.
+    #[account(mut)]
+    pub profile: Account<'info, Profile>,
+}
+
+/// The profile of each account on Gappe. This is what is other users see before
+/// interacting with others.
+#[account]
+#[derive(Default)]
+pub struct Profile {
+    pub username: String,
+}
+
