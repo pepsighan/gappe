3593b48c126c19426450b0c320751ca78e8c13b1
Sharad Chand <pepsighan@sharadchand.com>
2022-03-04 13:41:28 +0530

    Add a test

--- tests/gappe.ts
+++ tests/gappe.ts
@@ -1,17 +1,23 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Gappe } from '../target/types/gappe';
+import {Program} from '@project-serum/anchor';
+import {Gappe} from '../target/types/gappe';
 
 describe('gappe', () => {
-
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Gappe as Program<Gappe>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('setup profile', async () => {
+    const account = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.setupProfile('username', {
+      accounts: {
+        profile: account.publicKey
+      },
+      signers: [account]
+    });
+
+    // Check whether the username is set.
   });
 });
