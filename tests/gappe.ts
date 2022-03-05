b391cfe5a8a3f3390116b307982caa251e718531
Sharad Chand <pepsighan@sharadchand.com>
2022-03-05 13:31:38 +0530

    Check if data stored in separate account

--- tests/gappe.ts
+++ tests/gappe.ts
@@ -10,44 +10,51 @@ describe('gappe', () => {
 
   const program = anchor.workspace.Gappe as Program<Gappe>;
 
-  it('setup profile', async () => {
-    const account = anchor.web3.Keypair.generate();
+  const account1 = anchor.web3.Keypair.generate();
+  const account2 = anchor.web3.Keypair.generate();
 
+  it('setup profile', async () => {
     await program.rpc.setupProfile('username', {
       accounts: {
-        profile: account.publicKey,
+        profile: account1.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       },
-      signers: [account],
+      signers: [account1],
     });
 
-    const profile = await program.account.profile.fetch(account.publicKey);
+    const profile = await program.account.profile.fetch(account1.publicKey);
     expect(profile.username).to.equal('username');
   });
 
   it('update profile', async () => {
-    const account = anchor.web3.Keypair.generate();
-
     await program.rpc.setupProfile('username', {
       accounts: {
-        profile: account.publicKey,
+        profile: account2.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       },
-      signers: [account],
+      signers: [account2],
     });
 
-    let profile = await program.account.profile.fetch(account.publicKey);
+    let profile = await program.account.profile.fetch(account2.publicKey);
     expect(profile.username).to.equal('username');
 
     await program.rpc.updateUsername('username2', {
       accounts: {
-        profile: account.publicKey,
+        profile: account2.publicKey,
       },
     });
 
-    profile = await program.account.profile.fetch(account.publicKey);
+    profile = await program.account.profile.fetch(account2.publicKey);
     expect(profile.username).to.equal('username2');
+  });
+
+  it('fetch two separate profile', async () => {
+    const profile1 = await program.account.profile.fetch(account1.publicKey);
+    expect(profile1.username).to.equal('username');
+
+    const profile2 = await program.account.profile.fetch(account2.publicKey);
+    expect(profile2.username).to.equal('username2');
   });
 });
