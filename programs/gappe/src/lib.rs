05eeb85f3895988392d8c545d03914c8e0fbc505
Sharad Chand <pepsighan@sharadchand.com>
2022-03-05 23:47:33 +0530

    Add func to send friend requests

--- programs/gappe/src/lib.rs
+++ programs/gappe/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate core;
+
+use core::num::flt2dec::Sign;
 use anchor_lang::prelude::*;
 
 declare_id!("7spG7C84YJyTaxVvHLLtNbWF18SgACLyz5AdkBrwU1zY");
@@ -16,6 +19,28 @@ pub mod gappe {
     /// Updates the name of a profile.
     pub fn update_name(ctx: Context<UpdateName>, name: String) -> ProgramResult {
         ctx.accounts.profile.name = name;
+        Ok(())
+    }
+
+    /// Sends friend request.
+    pub fn send_friend_request(ctx: Context<SendFriendRequest>) -> ProgramResult {
+        let self_pubkey = ctx.accounts.requester.key();
+        let friend_pubkey = ctx.accounts.profile_friend.authority;
+
+        // If already a friend, error out.
+        if ctx.accounts.profile_friend.friends.contains(&self_pubkey) {
+            return Err(error!(GappeError::AlreadyFriend));
+        }
+
+        // If already requested, ignore this one.
+        if ctx.accounts.profile_friend.friend_requests.contains(&self_pubkey) {
+            return Ok(());
+        }
+
+        // Send the request to a potential friend.
+        ctx.accounts.profile_friend.friend_requests.push(self_pubkey);
+        // Register the other user as one's friend.
+        ctx.accounts.profile_self.friends.push(friend_pubkey);
         Ok(())
     }
 }
@@ -40,6 +65,18 @@ pub struct UpdateName<'info> {
     pub authority: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct SendFriendRequest<'info> {
+    /// The profile of a potential friend.
+    #[account(mut)]
+    pub profile_friend: Account<'info, Profile>,
+    /// The profile of oneself.
+    #[account(mut)]
+    pub profile_self: Account<'info, Profile>,
+    /// The one who is sending the request.
+    pub requester: Signer<'info>
+}
+
 /// The profile of each account on Gappe. This is what is other users see before
 /// interacting with others.
 #[account]
@@ -53,4 +90,11 @@ pub struct Profile {
     pub friends: Vec<Pubkey>,
     /// All the friend requests sent to this profile.
     pub friend_requests: Vec<Pubkey>,
+}
+
+
+#[error_code]
+pub enum GappeError {
+    #[msg("Already a friend")]
+    AlreadyFriend,
 }
