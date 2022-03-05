0f742ba92331e60c499e1c3574ae337137a7a2a5
Sharad Chand <pepsighan@sharadchand.com>
2022-03-05 13:15:18 +0530

    Check if the username is properly set

--- tests/gappe.ts
+++ tests/gappe.ts
@@ -1,6 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { Gappe } from '../target/types/gappe';
+import { expect } from 'chai';
 
 describe('gappe', () => {
   // Configure the client to use the local cluster.
@@ -12,7 +13,7 @@ describe('gappe', () => {
   it('setup profile', async () => {
     const account = anchor.web3.Keypair.generate();
 
-    const tx = await program.rpc.setupProfile('username', {
+    await program.rpc.setupProfile('username', {
       accounts: {
         profile: account.publicKey,
         user: provider.wallet.publicKey,
@@ -21,6 +22,7 @@ describe('gappe', () => {
       signers: [account],
     });
 
-    // Check whether the username is set.
+    const profile = await program.account.profile.fetch(account.publicKey);
+    expect(profile.username).to.equal('username');
   });
 });
