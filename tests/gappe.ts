8cc40657c4c52c27d9e0e75938bd2cc06ae851fa
Sharad Chand <pepsighan@sharadchand.com>
2022-03-07 14:35:27 +0530

    Implement sending message

--- tests/gappe.ts
+++ tests/gappe.ts
@@ -114,4 +114,31 @@ describe('gappe', () => {
       other.publicKey.toBase58()
     );
   });
+
+  it('send a message', async () => {
+    const user = anchor.web3.Keypair.generate();
+    const other = anchor.web3.Keypair.generate();
+    const message = anchor.web3.Keypair.generate();
+
+    const signature = await program.provider.connection.requestAirdrop(
+      user.publicKey,
+      100000000
+    );
+    await program.provider.connection.confirmTransaction(signature);
+
+    await program.rpc.sendMessage('Hello there!', other.publicKey, {
+      accounts: {
+        message: message.publicKey,
+        owner: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [user, message],
+    });
+
+    const savedMessage = await program.account.message.fetch(message.publicKey);
+    expect(savedMessage.text).to.be.equal('Hello there!');
+    expect(savedMessage.sentTo.toBase58()).to.be.equal(
+      other.publicKey.toBase58()
+    );
+  });
 });
