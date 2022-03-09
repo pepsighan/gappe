04693355dba0febec8068535e7e10066187592a6
Sharad Chand <pepsighan@sharadchand.com>
2022-03-09 14:04:09 +0530

    Remove the profile (#6)

--- programs/gappe/src/lib.rs
+++ programs/gappe/src/lib.rs
@@ -8,19 +8,6 @@ declare_id!("3qFjQjHysMD7vDkpC7DBAyUhNkMQEPYRVWzkokZHLQfg");
 pub mod gappe {
     use super::*;
 
-    /// Setup profile for an account.
-    pub fn setup_profile(ctx: Context<SetupProfile>, name: String, authority: Pubkey) -> Result<()> {
-        ctx.accounts.profile.name = name;
-        ctx.accounts.profile.authority = authority;
-        Ok(())
-    }
-
-    /// Updates the name of a profile.
-    pub fn update_name(ctx: Context<UpdateName>, name: String) -> Result<()> {
-        ctx.accounts.profile.name = name;
-        Ok(())
-    }
-
     /// Sends a message.
     pub fn send_message(ctx: Context<SendMessage>, message: String, sent_to: Pubkey) -> Result<()> {
         ctx.accounts.message.text = message;
@@ -29,38 +16,6 @@ pub mod gappe {
         ctx.accounts.message.timestamp = Clock::get().unwrap().unix_timestamp;
         Ok(())
     }
-}
-
-#[derive(Accounts)]
-pub struct SetupProfile<'info> {
-    /// Limits the size of profile to 8000 bytes. Since `String` name is open-ended,
-    /// a size needs to be provided.
-    #[account(init, payer = user, space = 8000)]
-    pub profile: Account<'info, Profile>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct UpdateName<'info> {
-    /// Only allows the actual wallet of the profile to update profile.
-    /// Checks profile.authority matches signer's key.
-    #[account(mut, has_one = authority)]
-    pub profile: Account<'info, Profile>,
-    pub authority: Signer<'info>,
-}
-
-
-/// The profile of each account on Gappe. This is what is other users see before
-/// interacting with others.
-#[account]
-#[derive(Default)]
-pub struct Profile {
-    /// The wallet which owns this profile and can make changes to it.
-    pub authority: Pubkey,
-    /// The name of the user.
-    pub name: String,
 }
 
 #[derive(Accounts)]
